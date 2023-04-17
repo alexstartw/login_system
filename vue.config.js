e8a5b86fc2beed3fc0d2f3457e64471c7da4c8d8
Alex <alexstartw@gmail.com>
2023-04-17 13:46:41 +0800

    change back

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
 })
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/login_system/public/'
+      ? '/login_system/'
       : '/'
 }
