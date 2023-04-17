c3594e93e5e03c0586e676eb45aaaee3e2ba59f8
Alex <alexstartw@gmail.com>
2023-04-17 12:54:50 +0800

    Initial commit

---
+++ src/components/LoginView.vue
@@ -0,0 +1,32 @@
+﻿<template>
+  <form name="login-form" >
+    <div class="mb-3">
+      <label for="username">Username: </label>
+      <input type="text" id="username" v-model="input.username" />
+    </div>
+    <div class="mb-3">
+      <label for="password">Password: </label>
+      <input type="password" id="password" v-model="input.password" />
+    </div>
+    <button class="btn btn-outline-dark" type="submit">
+      Login
+    </button>
+
+    <p>Username is: {{input.username}}</p>
+    <p>Password is: {{input.password}}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  data(){
+    return{
+      input:{
+        username: "",
+        password: ""
+      }
+    }
+  }
+}
+</script>
